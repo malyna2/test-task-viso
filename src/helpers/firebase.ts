7bae38ab163115d038c0c4826023366e8e0f4795
Andrii Malynovskyi <57286096+malyna2@users.noreply.github.com>
2024-07-23 19:22:12 +0300

    typo

--- src/helpers/firebase.ts
+++ src/helpers/firebase.ts
@@ -1,10 +1,6 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import {getDatabase} from 'firebase/database'
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyC9t2S8tYnTeMalMYFaDMkmBk5qwMIOvSM",
   authDomain: "viso-44549.firebaseapp.com",
@@ -15,7 +11,6 @@ const firebaseConfig = {
   appId: "1:807028201094:web:5d94e9e629f007f5fa2307"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const database = getDatabase(app);
 export {database}
--- src/helpers/loadGoogleMapsScript.ts
+++ src/helpers/loadGoogleMapsScript.ts
@@ -1,12 +1,10 @@
 export const loadGoogleMapsScript = (apiKey: string): Promise<void> => {
     return new Promise((resolve, reject) => {
       if (window.google && window.google.maps) {
-        // Google Maps script already loaded
         resolve();
         return;
       }
   
-      // Create a script element to load Google Maps API
       const script = document.createElement('script');
       script.src = `https://maps.googleapis.com/maps/api/js?key=${apiKey}&libraries=places,geometry`;
       script.async = true;
